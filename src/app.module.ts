881ccc8d9f76082b63b9c6e2011c5fb69e27fce8
Shudipta Das <shudiptod@gmail.com>
2022-11-18 04:05:42 +0600

    database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Event } from './typeorm/entities/Event';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      database: 'event_manager',
+      username: 'root',
+      password: 'root',
+      entities: [Event],
+      synchronize: true,
+    }),
+    EventsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
